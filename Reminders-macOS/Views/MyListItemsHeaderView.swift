f43e798d03ccd07628822823b7968bf49042b1b8
Farhana <farhana.swe@gmail.com>
2024-04-27 20:36:43 -0700

    create MyListItemsHeaderView

---
+++ Reminders-macOS/Views/MyListItemsHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  MyListItemsHeaderView.swift
+//  Reminders-macOS
+//
+//  Created by Farhana Mustafa on 4/27/24.
+//
+
+import SwiftUI
+
+struct MyListItemsHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyListItemsHeaderView()
+}
