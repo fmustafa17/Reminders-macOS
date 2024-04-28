fcb4928d39deb6f798f988f42fd03c444b42b168
Farhana <farhana.swe@gmail.com>
2024-04-27 21:31:07 -0700

    Update DueDate.swift

--- Reminders-macOS/Models/DueDate.swift
+++ Reminders-macOS/Models/DueDate.swift
@@ -13,3 +13,23 @@ enum DueDate {
     case yesterday
     case custom(Date)
 }
+
+extension DueDate {
+    var value: Date {
+        switch self {
+        case .today:
+            return Date.today
+        case .tomorrow:
+            return Date.tomorrow
+        case .yesterday:
+            return Date.yesterday
+        case .custom(let date):
+            return date
+        }
+    }
+
+    var title: String {
+        self.value.formatAsString
+    }
+
+}
