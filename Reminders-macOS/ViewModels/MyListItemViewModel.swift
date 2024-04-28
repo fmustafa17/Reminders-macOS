d6e94eac5c21ae4b518034b0e1de41925c5604b2
Farhana <farhana.swe@gmail.com>
2024-04-27 21:12:51 -0700

    Update MyListItemViewModel.swift

--- Reminders-macOS/ViewModels/MyListItemViewModel.swift
+++ Reminders-macOS/ViewModels/MyListItemViewModel.swift
@@ -5,4 +5,18 @@
 //  Created by Farhana Mustafa on 4/27/24.
 //
 
+import CoreData
 import Foundation
+
+struct MyListItemViewModel: Identifiable {
+    private let myListItem: MyListItem
+    var id = UUID().uuidString
+    
+    init(myListItem: MyListItem) {
+        self.myListItem = myListItem
+    }
+    
+    var title: String {
+        myListItem.title ?? ""
+    }
+}
