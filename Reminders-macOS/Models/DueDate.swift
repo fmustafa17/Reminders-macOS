5703801bfdc310ce7fe08acbd370113767c402fa
Farhana <farhana.swe@gmail.com>
2024-04-27 21:18:56 -0700

    add duedate and date extensions

---
+++ Reminders-macOS/Models/DueDate.swift
@@ -0,0 +1,15 @@
+//
+//  DueDate.swift
+//  Reminders-macOS
+//
+//  Created by Farhana Mustafa on 4/27/24.
+//
+
+import Foundation
+
+enum DueDate {
+    case today
+    case tomorrow
+    case yesterday
+    case custom(Date)
+}
